533461432a420fbfe6d483d3f2dd9c851f6196d2
André Alves Correia <andre2013aguabranca@gmail.com>
2025-02-21 07:49:15 -0300

    chore: create components

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,28 @@
 import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { ArrowRight, Copy, Mail } from 'lucide-react'
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-4xl font-semibold font-heading">Hello world!</h1>
-      <Button />
+      <Button type="submit">
+        Oi
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+      <div>
+        <InputRoot error>
+          <InputField placeholder="Digite o e-mail" />
+          <InputIcon>
+            <Mail />
+          </InputIcon>
+        </InputRoot>
+      </div>
     </main>
   );
 }
--- web/src/components/button.tsx
+++ web/src/components/button.tsx
@@ -1,3 +1,11 @@
-export function Button() {
-  return <button className="px-5 py-2 rounded-sm blue">Botao</button>
+import { ComponentProps } from 'react'
+
+interface ButtonProps extends ComponentProps<'button'> {
+}
+
+export function Button(props: ButtonProps) {
+  return <button
+    className="flex justify-between items-center px-5 h-12 w-full bg-gray-500 text-blue font-semibold rounded-xl cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+    {...props}
+  />
 }
