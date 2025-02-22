85eb22ae648ea6cff1a4223650dbde06d426036d
André Alves Correia <andre2013aguabranca@gmail.com>
2025-02-22 19:41:49 -0300

    chore: create api calls using orval

---
+++ web/orval.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'orval'
+export default defineConfig({
+  api: {
+    input: 'http://localhost:3333/docs/json',
+    output: {
+      target: './src/http/api.ts',
+      client: 'fetch',
+      httpClient: 'fetch',
+      clean: true, // apaga o arquivo anterior ao gerar um novo.
+      baseUrl: 'http://localhost:3333',
+      override: {
+        fetch: {
+          includeHttpResponseReturnType: false, // por padrão o orval pega os status code (ex: 200, 403, etc). Essa opção desativa isso.
+        },
+      },
+    },
+  },
+})
+
+// npx orval -> gera a tipagem em typescript de todas as rotas do backend, a partir da documentação swagger. Necessário que o backend esteja em execução antes de executar este comando.
